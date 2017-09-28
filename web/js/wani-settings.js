06ee5221eff51c0240d3147cada9b2ff3ab41b8b
Jakeoid <jake.triumphant@gmail.com>
2017-09-28 23:05:23 +1000

    feat(wani-settings.js): Make-do versions system.

--- web/js/wani-settings.js
+++ web/js/wani-settings.js
@@ -3,15 +3,37 @@ var token = "";
 var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
 var isFirefox = window.navigator.userAgent.match(/Firefox\/([0-9]+)\./);
 
-if (isChrome) {
-    chrome.storage.sync.get(['api_key'], function (items) {
-        token = items.api_key;
-    });
-} else {
-    token = window.localStorage.getItem(['api_key'])
+var version = chrome.app.getDetails().version;
+var latest_version = "";
+
+// https://api.github.com/repos/bleeps-n-bloops/newtab-wanikani/releases
+
+
+function _set_version(version) {
+    latest_version = version;
+    $('#latest-version').text(latest_version);
 }
 
-$('#wanikey').val(token)
+function get_latest_version() {
+
+    var API = "https://api.github.com/repos/bleeps-n-bloops/newtab-wanikani/releases";
+    
+    var version = "0.0.0";
+
+    var status = $.ajax({
+        url: API,
+        dataType: 'jsonp',
+        success: function(data, status){
+            version = data.data[0]['tag_name']
+            return version
+        },
+        error: function() {
+            return '?.?.?'
+        }
+    })
+    .then(_set_version(version))
+
+}
 
 function save_form() {
     if (isChrome) {
@@ -21,9 +43,27 @@ function save_form() {
     } else {
         window.localStorage.setItem("api_key", $('#wanikey').val())
     }
+
+    alert('Thanks! Your settings have been saved!')
 }
+
 $(document).ready(function () {
+
+    if (isChrome) {
+        chrome.storage.sync.get(['api_key'], function (items) {
+            token = items.api_key;
+        });
+    } else {
+        token = window.localStorage.getItem(['api_key'])
+    }
+
+    $('#wanikey').val(token);
+    
+    $('#user-version').text(version);
+    $('#footer-version').text(version);
+
     $("#save-settings").on("click", function () {
         save_form();
     });
+
 });
